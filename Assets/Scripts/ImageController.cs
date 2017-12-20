521ab6419d16c127edc77c82e93217c6275f07f8
Nick Brander <allan.firewind@gmail.com>
2017-12-19 19:31:29 -0600

    Added logic for splash screen timing and switching to game start

---
+++ Assets/Scripts/ImageController.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ImageController : MonoBehaviour {
+
+	public float FadeRate;
+
+	private Image image;
+	private float targetAlpha;
+	private	float alphaDiff;
+	private Color curColor;
+	private LevelManager levelManager;
+	private bool isReady;
+
+	// Use this for initialization
+	void Start () {
+
+		//Initial Definitions
+		levelManager = GameObject.FindObjectOfType<LevelManager> ();
+		image = this.GetComponent<Image>();
+		targetAlpha = this.image.color.a;
+		curColor = this.image.color;
+
+		if(this.image==null)
+		{
+			Debug.LogError("Error: No image on "+this.name);
+		}
+
+		//Image initialization
+		FadeIn ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		alphaDiff = Mathf.Abs (curColor.a - this.targetAlpha);
+
+		if (alphaDiff>0.0001f)
+		{
+			curColor.a = Mathf.Lerp(curColor.a,targetAlpha,FadeRate*Time.deltaTime);
+			this.image.color = curColor;
+		}
+
+		if (curColor.a >= 0.8f) {
+			FadeOut();
+		} 
+
+		if (isReady == true && curColor.a <= 0.05f) {
+			levelManager.LoadNextLevel();
+		}
+	}
+	
+	public void FadeOut()
+	{
+		this.targetAlpha = 0.0f;
+		isReady = true;
+	}
+	
+	public void FadeIn()
+	{
+		this.targetAlpha = 1.0f;
+		isReady = false;
+	}
+}
---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel (string name) {
+		Application.LoadLevel (name);
+	}
+	public void PlayerDestroyed () {
+		LoadLoseLevel ();
+	}
+		public void PlayerWin () {
+		LoadWinLevel ();
+	}
+	public void QuitRequest () {
+		Debug.Log ("Quit Game");
+		Application.Quit ();
+	}
+	public void LoadNextLevel () {
+		Application.LoadLevel (Application.loadedLevel + 1);
+	}
+	void LoadLoseLevel () {
+		Application.LoadLevel ("Lose");
+	}
+
+	void LoadWinLevel () {
+		Application.LoadLevel ("Win");
+	}
+}
+
